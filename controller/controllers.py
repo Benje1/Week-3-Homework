280243704388e148126909bc81c678abf6ed8f3e
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-07-09 13:42:58 +0100

    done here

--- controller/controllers.py
+++ controller/controllers.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from unittest import result
+from flask import render_template, redirect, request
 from app import app
 from modules.players import Player
 from modules.roshambo import Game
@@ -7,8 +8,19 @@ from modules.roshambo import Game
 def index():
     return render_template('index.html')
 
+
 @app.route('/roshambo/<input_p1>/<input_p2>')
 def roshambo(input_p1, input_p2):
-    return render_template('result.html', result=roshambo(input_p1, input_p2))
-    # print(roshambo(input_p1, input_p2))
-    # return render_template('result.html', result=Game(player1, player2.roshambo(input_p1, input_p2)))
+    player_1 = Player('Player_1', input_p1)
+    player_2 = Player('Player_2', input_p2)
+    game = Game(player_1, player_2)
+    return render_template('result.html', result=game.roshambo(input_p1, input_p2))
+
+# @app.route('/roshambo', methods=["POST"])
+# def roshambo():
+#     input_p1 = request.form["input_p1"]
+#     player_1 = Player(player_1, input_p1)
+#     game = Game(player_1)
+#     return render_template(result=game.roshambo(input_p1))
+
+
--- modules/roshambo.py
+++ modules/roshambo.py
@@ -1,24 +1,58 @@
+import random
+
 class Game:
     def __init__ (self, _player1, _player2):
         self.player1 = _player1
         self.player2 = _player2
     
-    def roshambo(input_p1, input_p2):
-        if input_p1 == "rock" and input_p2 == "scissors":
-            return f"Player 1 wins by playing {input_p1}"
-        elif input_p1 == "rock" and input_p2 == "paper":
-            return f"Player 2 wins by playing {input_p2}"
-        elif input_p1 == "paper" and input_p2 == "rock":
-            return f"Player 1 wins by playing {input_p1}"
-        elif input_p1 == "paper" and input_p2 == "scissors":
-            return f"Player 2 wins by playing {input_p2}"
-        elif input_p1 == "scissors" and input_p2 == "paper":
-            return f"Player 1 wins by playing {input_p1}"
-        else:
-            if input_p1 == "scissors" and input_p2 == "rock":
-                return f"Player 2 wins by playing {input_p2}"
+    def roshambo(self, p1_input, p2_input):
+        # p2_input = random.randint(1,3)
+        # if p2_input == 1:
+        #     p2_input = "rock"
+        # elif p2_input == 2:
+        #     p2_input = "paper"
+        # elif p2_input == 3:
+        #     p2_input = "scissor"
+        if p1_input == "rock":
+            if p2_input == "rock":
+                return None
+            elif p2_input == "paper":
+                return f"Player 2 wins"
+            elif p2_input == "scissor":
+                return f"Player 1 wins"
+        elif p1_input == "paper":
+            if p2_input == "paper":
+                return None
+            elif p2_input == "rock":
+                return f"Player 2 wins"
+            elif p2_input == "scissor":
+                return f"Player 1 wins"
+        elif p1_input == "scissor":
+            if p2_input == "scissor":
+                return None
+            elif p2_input == "paper":
+                return f"Player 2 wins"
+            elif p2_input == "rock":
+                return f"Player 1 wins"
+
+
+
+    # def roshambo(self, input_p1, input_p2):
+    #     if input_p1 == "rock" and input_p2 == "scissors":
+    #         return f"Player 1 wins by playing {input_p1}"
+    #     elif input_p1 == "rock" and input_p2 == "paper":
+    #         return f"Player 2 wins by playing {input_p2}"
+    #     elif input_p1 == "paper" and input_p2 == "rock":
+    #         return f"Player 1 wins by playing {input_p1}"
+    #     elif input_p1 == "paper" and input_p2 == "scissors":
+    #         return f"Player 2 wins by playing {input_p2}"
+    #     elif input_p1 == "scissors" and input_p2 == "paper":
+    #         return f"Player 1 wins by playing {input_p1}"
+    #     else:
+    #         if input_p1 == "scissors" and input_p2 == "rock":
+    #             return f"Player 2 wins by playing {input_p2}"
     
-    # def rock_vs_rock(input_p1, input_p2):
+    # # def rock_vs_rock(input_p1, input_p2):
     #     if input_p1 == "rock" and input_p2 == "rock":
     #         return None
     
@@ -30,25 +64,3 @@ class Game:
     #     if input_p1 == "scissors" and input_p2 == "scissors":
     #         return None
     
-    # def roshambo(p1_input, p2_input):
-    #     if p1_input == "rock":
-    #         if p2_input == "rock":
-    #             return None
-    #         elif p2_input == "paper":
-    #             return f"Player 2 wins"
-    #         elif p2_input == "scissors":
-    #             return f"Player 1 wins"
-    #     elif p1_input == "paper":
-    #         if p2_input == "paper":
-    #             return None
-    #         elif p2_input == "rock":
-    #             return f"Player 2 wins"
-    #         elif p2_input == "scissor":
-    #             return f"Player 1 wins"
-    #     elif p1_input == "scissor":
-    #         if p2_input == "scissor":
-    #             return None
-    #         elif p2_input == "paper":
-    #             return f"Player 2 wins"
-    #         elif p2_input == "rock":
-    #             return f"Player 1 wins"
